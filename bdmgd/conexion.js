0610485abba19b892c5a4ecd1f868d7eafde881d
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-03-24 14:05:06 -0300

    cambio
    cambio

--- bdmgd/conexion.js
+++ bdmgd/conexion.js
@@ -1,22 +1,28 @@
 var mongoose = require('mongoose');
+var crypto = require('../util-implements/cryptojs-implement');
 //var mail = require('../../util-implements/mail/configuracion/conf');
 var conexion = {
     conectar: conectar,
     descontectar:descontectar
 }
 var dconect= process.env.conectString;
+
+
 function conectar() {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(dconect, { useMongoClient: true, promiseLibrary: global.Promise }, (err) => {
-            if (err != null) {
-
-                resolve({ error: true });
-
-            } else {
-                resolve({ error: false, type: err });
-            }
-        });
+    crypto.decode(dconect).then((dd)=>{
+        return new Promise((resolve, reject) => {
+            mongoose.connect(dd, { useMongoClient: true, promiseLibrary: global.Promise }, (err) => {
+                if (err != null) {
+    
+                    resolve({ error: true });
+    
+                } else {
+                    resolve({ error: false, type: err });
+                }
+            });
+        })
     })
+    
 
 }
 
